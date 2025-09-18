9544b3e9b88e4dd525b41aa413524be9ee8ffc6c
jannatisamania <sabbitbindu3@hmail.com>
2025-09-18 23:02:33 +0600

    heart count completed

--- home.js
+++ home.js
@@ -1,1 +1,12 @@
- console.log('i am missing my husband')
+/* heart count  */
+const allHeart = document.querySelectorAll('.heart-icon') ;
+
+  for( let heart of allHeart){
+    heart.addEventListener('click' , function(){
+     
+     let heartCount = parseInt(document.getElementById('heart-count').innerText) ;
+     heartCount++;
+     document.getElementById('heart-count').innerText = heartCount ;
+    })
+  }
+
